33d7a809f919691ac69de2258473724070b8fe3c
arvein <moonflower019@gmail.com>
2020-06-03 13:43:21 +0300

    create input search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const AutoComplete = () => {
+    return (
+        <div className='lex-container flex-column pos-rel'>
+            <input id='auto' placeholder='Type something to search'/>
+        </div>
+    );
+}
 
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='App'>
+            <div>
+                <h2><i className='fa fa-search' aria-hidden='true'></i> Search your pokemon</h2>
+            </div>
+            <div className='autocomplete-container'>
+                <AutoComplete/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
