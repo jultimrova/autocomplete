18accc4dbfc508123900f708a1f4fe7d64967ffd
arvein <moonflower019@gmail.com>
2020-06-03 13:59:34 +0300

    grab data from APi and put them to array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const AutoComplete = () => {
+    const [display, setDisplay] = useState(false);
+    const [data, setData] = useState([]);
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        const pokemon = [];
+        const promises = new Array(35)
+            .fill()
+            .map((v, i) => fetch(`https://pokeapi.co/api/v2/pokemon-form/${i + 1}`));
+        Promise.all(promises).then(pokemonArray => {
+            return pokemonArray.map(res =>
+                res.json().then(({name, sprites: {front_default: sprite}}) =>
+                    pokemon.push({name, sprite})
+                )
+            );
+        });
+        setData(pokemon);
+    }, []);
+
+
     return (
         <div className='lex-container flex-column pos-rel'>
             <input id='auto' placeholder='Type something to search'/>
