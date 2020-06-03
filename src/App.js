198d3134697824bbc3036dd77bc6713b21dfb0a7
arvein <moonflower019@gmail.com>
2020-06-03 16:16:41 +0300

    try to add keyboard usage and debounce

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, {useEffect, useRef, useState} from 'react';
+import {useDebounce} from 'use-debounce';
 import './App.css';
 
 const AutoComplete = () => {
     const [display, setDisplay] = useState(false);
     const [data, setData] = useState([]);
     const [search, setSearch] = useState('');
+    const [isSearching, setIsSearching] = useState(false);
+    const [highlightedId, setHighlightedId] = useState(0);
     const wrapperRef = useRef(null);
+
+    const debouncedSearchTerm = useDebounce(search, 500);
 
     useEffect(() => {
         const pokemon = [];
         const promises = new Array(35)
             .fill()
             .map((v, i) => fetch(`https://pokeapi.co/api/v2/pokemon-form/${i + 1}`));
+
         Promise.all(promises).then(pokemonArray => {
             return pokemonArray.map(res =>
                 res.json().then(({name, sprites: {front_default: sprite}}) =>
@@ -19,6 +25,7 @@ const AutoComplete = () => {
                 )
             );
         });
+
         setData(pokemon);
     }, []);
 
@@ -28,6 +35,40 @@ const AutoComplete = () => {
             window.removeEventListener("mousedown", handleClickOutside);
         };
     }, []);
+
+    const handleKeyDown = (event) => {
+        const UP = 38;
+        const DOWN = 40;
+        const ENTER = 13;
+        const INITIAL_ID = 0;
+
+        if (event.keyCode === DOWN) {
+            event.preventDefault();
+
+            const id = highlightedId;
+            const nextId = id !== undefined ? id + 1 : INITIAL_ID;
+
+            if (nextId < data.length) {
+                setHighlightedId(nextId);
+            } else setHighlightedId(INITIAL_ID);
+        }
+
+        if (event.keyCode === UP) {
+            event.preventDefault();
+
+            const lastId = data.length - 1;
+            const id = highlightedId;
+            const prevId = id !== undefined ? id - 1 : lastId;
+
+            if (prevId >= 0) {
+                setHighlightedId(prevId);
+            } else setHighlightedId(lastId);
+        }
+
+        if (event.keyCode === ENTER && highlightedId !== undefined) {
+            setPokemonSearch(highlightedId);
+        }
+    }
 
     const handleClickOutside = event => {
         const {current: wrap} = wrapperRef;
@@ -56,7 +97,11 @@ const AutoComplete = () => {
                    value={search}
                    onClick={onClickDisplay}
                    onChange={onChangeSearch}
+                   onKeyDown={handleKeyDown}
             />
+
+            {isSearching && <div>Searching...</div>}
+
             {display && (
                 <div className='autocompleteContainer'>
                     {data
@@ -67,7 +112,7 @@ const AutoComplete = () => {
                                      key={id}
                                      tabIndex='0'
                                      onClick={() => setPokemonSearch(value.name)}
-                                >
+                                     aria-selected={highlightedId === id}>
                                     <span>{value.name}</span>
                                     <img src={value.sprite} alt="pokemon"/>
                                 </div>
